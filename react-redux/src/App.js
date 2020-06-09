f8a9f4ed6da07cf8b592ff4dad265360acf94cbc
DanielleInkster <danielle.inkster@gmail.com>
2020-06-09 01:27:33 -0600

    AgeUp works

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  
+    state = {
+      age: 21
+    }
+
+    onAgeUp=()=>{
+      let age = this.state.age
+      this.setState({
+        age: ++age
+      })
+    }
+
+  render () {
+    return (
       <div className="App">
-        <div>Age: <span></span></div>
-        <button>Age Up</button>
-        <button>Age Down</button>
+        <div className="App">
+          <div>Age: <span>{this.state.age}</span></div>
+          <button onClick={this.onAgeUp}>Age Up</button>
+          <button>Age Down</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
